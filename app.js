db34dbe49f706983a502800106e0cd3ec83bfba0
Ngoc <ngocb1910551@student.ctu.edu.vn>
2023-02-17 09:25:39 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/route/contact.route");
 
 const app = express();
 
@@ -11,5 +12,7 @@ app.get("/", (req, res) => {
         message : "Welcome to contact book application."
     });
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
